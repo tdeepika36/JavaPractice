174f8d0eecd2306b4db57aa3d020b24396374669
tdeepika36 <tdeepu2508@gmail.com>
2026-04-07 09:26:28 -0500

    comment

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -2,8 +2,7 @@ package org.example;
 
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Program {
     public static void main(String args[]) {
 
