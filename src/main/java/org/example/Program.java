4c12788887f11ed52a24691be73aafcc0037c6de
tdeepika36 <tdeepu2508@gmail.com>
2026-04-07 09:22:31 -0500

    main

--- src/main/java/org/example/Program.java
+++ src/main/java/org/example/Program.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class Program {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
