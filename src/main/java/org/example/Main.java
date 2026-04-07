53cec535b433a51c73e1c6a08f97403a71fdea25
tdeepika36 <tdeepu2508@gmail.com>
2026-04-07 09:20:07 -0500

    main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,43 @@
 package org.example;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+    public static void main(String args[]) {
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of an array" + " ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.println("Enter numbers" + " ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int k = 0; k < n; k++) {
+            System.out.print(arr[k] + " ");
+        }
+        System.out.println();
+        SortArray(arr);
+        for(int q=0;q<n;q++) {
+            System.out.print(arr[q]+" ");
         }
     }
-}
+    public static int[] SortArray(int array []){
+        for(int i=0;i<array.length;i++)
+        {
+            for(int j=0;j<array.length;j++ )
+            {
+                if(array[i]<array[j]) //ascending
+                {
+                    int temp=array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+       return array;
+
+        }
+    }
