a2e97e134a64499ba137e48d153e260c9a5441bd
tdeepika36 <tdeepu2508@gmail.com>
2026-04-07 09:18:22 -0500

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void SortingNums() {
+        Main obj=new Main();
+        int[] Array={6,5,4,3,2,1};
+        int[] expectedArray = {1,2,3,4,5,6};
+        int[] actualArray  = obj.SortArray(Array);
+        assertArrayEquals(expectedArray, actualArray);
+
+    }
+}
