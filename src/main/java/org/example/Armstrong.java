d52eece92a1ec72220ad201f8ad75685a34ac8a4
tdeepika36 <tdeepu2508@gmail.com>
2026-04-16 12:11:42 -0500

    logback with slf4j

--- src/main/java/org/example/Armstrong.java
+++ src/main/java/org/example/Armstrong.java
@@ -1,38 +1,36 @@
 package org.example;
-//import org.utils;
 
-import org.apache.logging.log4j.Level;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import org.apache.logging.log4j.LogManager;
-import org.utils.Log;
 
 
 //8208,371,153
 public class Armstrong{
 
-
+    private static final Logger LOGGER= LoggerFactory.getLogger(Armstrong.class);
 
     public boolean isArmstrong(int n){
         int length=0;
         int p=n;
         int q=n;
-        Log.info("getting length of given number");
+        LOGGER.info("given number {}",n);
         while(n>0)
         {
             n=n/10;
             length++;
         }
         int sum=0;
-        Log.info("adding the digits of number");
+        LOGGER.info("length of number {}",length);
+
         while(p>0){
             int temp=0;
             temp=p%10;
             sum=(int)(sum+Math.pow(temp,length));
             p=p/10;
-           // LOGGER.log(Level.DEBUG,"debugging while loop");
         }
-        Log.info("checking if sum is equal to given number");
+        LOGGER.info("sum of digits,each raised to power of total number of digits {}",sum);
+        LOGGER.info("checking if sum {} is equal to given number {}",sum,q);
         if(sum==q)
            return true;
         else
